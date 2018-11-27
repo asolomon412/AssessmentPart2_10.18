33572d1231d944dbc4172531fbd015f248c64bdf
Antonella Solomon <antonellasolomon@gmail.com>
2018-11-27 10:27:42 -0500

    initial commit for part 2 final

---
+++ src/main/java/com/part2final/AssessmentPart2Final/AssessmentPart2FinalApplication.java
@@ -0,0 +1,12 @@
+package com.part2final.AssessmentPart2Final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentPart2FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentPart2FinalApplication.class, args);
+	}
+}
